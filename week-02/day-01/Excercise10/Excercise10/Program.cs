e295c6b7e531be270ab0fa1a8e668d8b49cf3160
Tamas <deaktamas93@gmail.com>
2017-09-11 15:07:52 +0200

    excercise done

---
+++ week-02/day-01/Excercise10/Excercise10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameUser = "Deak Tamas";
+            int ageUser = 24;
+            double heightUser = 1.88;
+            bool married = false;
+
+            Console.WriteLine("Name: " + nameUser);
+            Console.WriteLine("Age: " + ageUser);
+            Console.WriteLine("Height: " + heightUser + " cm");
+            Console.WriteLine("Married: " + married);
+            Console.ReadLine();
+       
+        }
+    }
+}
