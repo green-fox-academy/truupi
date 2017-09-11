036fee56dcb159be196e71c6efe9f96703690b29
Tamas <deaktamas93@gmail.com>
2017-09-11 16:15:02 +0200

    excercise done

--- week-02/day-01/Excercise11/Excercise11/Program.cs
+++ week-02/day-01/Excercise11/Excercise11/Program.cs
@@ -12,27 +12,31 @@ namespace Excercise11
         {
             int a = 3;
             // make it bigger by 10
-            a = a + 10;
+            a += 10;
 
             Console.WriteLine(a);
 
             int b = 100;
             // make it smaller by 7
+            b -= 7;
 
             Console.WriteLine(b);
 
             int c = 44;
             // please double c's value
+            c *= 2;
 
             Console.WriteLine(c);
 
             int d = 125;
             // please divide by 5 d's value
+            d /= 5;
 
             Console.WriteLine(d);
 
             int e = 8;
             // please cube of e's value
+            e *= e * e;
 
             Console.WriteLine(e);
 
@@ -40,24 +44,40 @@ namespace Excercise11
             int f2 = 345;
             // tell if f1 is bigger than f2 (print as a boolean)
 
+            Console.WriteLine(f1 > f2);
+
             int g1 = 350;
             int g2 = 200;
             // tell if the double of g2 is bigger than g1 (print as a boolean)
 
-            //int h = 1357988018575474;
+            Console.WriteLine((g2 * 2) > g1);
+
+            long h= 1357988018575474;
             // tell if it has 11 as a divisor (print as a boolean)
+
+            Console.WriteLine(h % 11 == 0);
 
             int i1 = 10;
             int i2 = 3;
+            double i3 = Math.Pow(i2, 2);
+            double i4 = Math.Pow(i2, 3);
+
             // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
+
+            Console.WriteLine(i1 > (i3) && i1 < i4);
 
             int j = 1521;
             // tell if j is dividable by 3 or 5 (print as a boolean)
 
+            Console.WriteLine(j % 3 == 0 || j % 5 == 0);
+
             string k = "Apple";
+
             //fill the k variable with its cotnent 4 times
 
-            Console.WriteLine(k);
+            string value = new string(' ', 4).Replace(" ", k);
+
+            Console.WriteLine(value);
 
             Console.ReadLine();
         }
