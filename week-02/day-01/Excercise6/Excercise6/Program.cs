d795a04de632bf934a5afde6e03e792217723e95
Tamas <deaktamas93@gmail.com>
2017-09-11 14:28:58 +0200

    excercise done

---
+++ week-02/day-01/Excercise6/Excercise6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daily = 6;
+            double semester = 17;
+            int workday = 5;
+            double weeklyavg = 52;
+            double allhours = semester * workday * daily;            
+
+
+            Console.WriteLine(allhours);
+
+            Console.WriteLine(allhours / (semester * weeklyavg) * 100 + "%");
+
+            Console.ReadLine();
+        }
+    }
+}
