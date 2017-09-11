ce362ce694863b01aa16cdbd48e73a3cb8b099d3
Tamas <deaktamas93@gmail.com>
2017-09-12 01:50:11 +0200

    triangle working

---
+++ week-02/day-01/triangle_ex28/triangle_ex28/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangle_ex28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string star = "";
+            Console.WriteLine("Enter here the size of your triangle:\n");
+            int input = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= input; i++)
+            {
+                Console.WriteLine(star);
+                star += "*";
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
