428219b1a9c65b4eebff5dd583c876368de13bce
Tamas <deaktamas93@gmail.com>
2017-09-11 14:55:51 +0200

    excercise done

---
+++ week-02/day-01/Excercise9/Excercise9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double massInKg = 84;
+            double heightInM = 1.88;
+            double bmi = massInKg / (heightInM * heightInM);
+
+            Console.WriteLine("My BMI index is: " + bmi);
+            Console.ReadLine();
+        }
+    }
+}
