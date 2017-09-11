8021fe867ba0d40b783d134db9887d50a04e71fe
Tamas <deaktamas93@gmail.com>
2017-09-11 13:51:14 +0200

    excercise done

---
+++ week-02/day-01/Excercise5/Excercise5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = 22;
+            double n2 = 13;
+            int n3 = 22;
+            int n4 = 13;
+
+            Console.WriteLine(n2 + n1);
+            Console.WriteLine(n1 - n2);
+            Console.WriteLine(n1 * n2);
+            Console.WriteLine(n1 / n2);
+            Console.WriteLine(n3 / n4);
+            Console.WriteLine(n3 % n4);
+
+            Console.ReadLine();
+        }
+    }
+}
