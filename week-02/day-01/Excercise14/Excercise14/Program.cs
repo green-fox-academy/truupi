6aa5fd63f9fc0accf03035173ac32d8687fe7c31
Tamas <deaktamas93@gmail.com>
2017-09-11 17:25:26 +0200

    excercise done

---
+++ week-02/day-01/Excercise14/Excercise14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the amount of km you want to convert to miles: ");
+            string userInputstring = Console.ReadLine();
+            double userInputdouble = Convert.ToDouble(userInputstring);
+            Console.WriteLine(userInputdouble + " km = " + userInputdouble * 0.621371192 + " miles");
+            Console.ReadLine();
+        }
+    }
+}
