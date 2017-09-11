68d60b1f46f4d68d4003b2986a1d43bae21d26b8
Tamas <deaktamas93@gmail.com>
2017-09-11 14:47:10 +0200

    excercise done

---
+++ week-02/day-01/Excercise8/Excercise8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 526;
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+
+        }
+    }
+}
