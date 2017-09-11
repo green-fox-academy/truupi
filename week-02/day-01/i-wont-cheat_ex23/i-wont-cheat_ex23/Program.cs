2e24cc1a6ace8e9344f1a5963395b3201dc542ac
Tamas <deaktamas93@gmail.com>
2017-09-12 00:51:53 +0200

    for loop configured

---
+++ week-02/day-01/i-wont-cheat_ex23/i-wont-cheat_ex23/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace i_wont_cheat_ex23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine("I won't cheat on the exam!");
+                }
+
+                Console.ReadLine();
+        }
+    }
+}
