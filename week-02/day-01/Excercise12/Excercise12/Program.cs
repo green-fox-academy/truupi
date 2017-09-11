19e1ebb69f4e6062d067a0db30de36c7ad802126
Tamas <deaktamas93@gmail.com>
2017-09-11 16:53:23 +0200

    excercise done

---
+++ week-02/day-01/Excercise12/Excercise12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How long the side of the cube shall be?  ");
+            string userInput = Console.ReadLine();
+            double num = Convert.ToDouble(userInput);
+
+            double surface = num * (Math.Pow(6, 2));
+            double volume = Math.Pow(num, 3);
+
+            Console.WriteLine("Surface of the cub: " + surface);
+            Console.WriteLine("Volume of the cub: " + volume);
+
+            Console.ReadLine();
+
+        }
+    }
+}
