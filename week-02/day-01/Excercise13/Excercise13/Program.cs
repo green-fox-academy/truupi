92b411dfba3e3c8105eb29b5b4301fbba4872490
Tamas <deaktamas93@gmail.com>
2017-09-11 17:14:00 +0200

    excercise done

---
+++ week-02/day-01/Excercise13/Excercise13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Greetings stranger! How can I call you? ");
+            string nameUser = Console.ReadLine();
+            Console.WriteLine("Well, hello " + nameUser + "! Have a nice day!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
