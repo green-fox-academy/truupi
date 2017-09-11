325dea820ff78b2c11d97f5e3f099ca7c47d9430
Tamas <deaktamas93@gmail.com>
2017-09-11 13:27:25 +0200

    excercise done

--- week-02/day-01/Excercise2/Excercise2/Program.cs
+++ week-02/day-01/Excercise2/Excercise2/Program.cs
@@ -10,7 +10,17 @@ namespace Excercise2
     {
         static void Main(string[] args)
         {
+            string line1 = "All the king's horses and all the king's men";
+            string line2 = "Humpty Dumpty had a great fall.";
+            string line3 = "Humpty Dumpty sat on a wall,";
+            string line4 = "Couldn't put Humpty together again.";
 
+            Console.WriteLine(line3);
+            Console.WriteLine(line2);
+            Console.WriteLine(line1);
+            Console.WriteLine(line4);
+
+            Console.ReadLine();
         }
     }
 }
