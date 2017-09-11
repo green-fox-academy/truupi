fe0994eaef5c272906f5e9274505cb8106b6344d
Tamas <deaktamas93@gmail.com>
2017-09-11 23:55:12 +0200

    odd/even calculator created

---
+++ week-02/day-01/odd-even_ex18/odd-even_ex18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd_even_ex18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let me tell you whether your number is even or odd. Enter your number: ");
+            string userInput = Console.ReadLine();
+            int userNum = Int32.Parse(userInput);
+
+            if ((userNum & 1) == 0)
+                Console.WriteLine("EVEN!\n");
+            else
+                Console.WriteLine("ODD!\n");
+
+            Console.ReadKey();
+        }
+    }
+}
