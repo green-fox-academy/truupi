17dcffaa5e9fb4fecdd0781e9f442b99cefb7e04
Tamas <deaktamas93@gmail.com>
2017-09-12 01:09:07 +0200

    counter with for loop added

--- week-02/day-01/print-even_ex24/print-even_ex24/Program.cs
+++ week-02/day-01/print-even_ex24/print-even_ex24/Program.cs
@@ -11,12 +11,22 @@ namespace print_even_ex24
         static void Main(string[] args)
         {
             int counter = 2;
-            Console.WriteLine("Even numbers between 0 to 500\n");
+            Console.WriteLine("Even numbers between 0 and 500 using while loop:\n");
 
             while (counter <= 500)
             {
                 Console.WriteLine(counter);
                 counter = counter + 2;
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("Even numbers between 0 and 500 using for loop:\n");
+
+            for (int i = 0; i <= 500; i++)
+            {
+                if (i%2 == 0)
+                Console.WriteLine(i);
             }
 
             Console.ReadLine();
