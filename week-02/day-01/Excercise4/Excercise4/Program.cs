46a270613ed8be19206fb29f9bfa7a40a33158a9
Tamas <deaktamas93@gmail.com>
2017-09-11 13:41:14 +0200

    excercise done

---
+++ week-02/day-01/Excercise4/Excercise4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Deak Tamas";
+            int age = 24;
+            double height = 1.88;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+
+            Console.ReadLine();
+        }
+    }
+}
