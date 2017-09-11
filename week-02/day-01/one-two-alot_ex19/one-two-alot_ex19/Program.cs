479e9977888fcb2093e5851af0712c5b5e0ff4f2
Tamas <deaktamas93@gmail.com>
2017-09-12 00:06:05 +0200

    one two alot calculator created

---
+++ week-02/day-01/one-two-alot_ex19/one-two-alot_ex19/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace one_two_alot_ex19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in a number: ");
+            string userInput = Console.ReadLine();
+            int userNum = Int32.Parse(userInput);
+            
+            if (userNum == 0)
+            {
+                Console.WriteLine("Not enough!");
+            }
+            else if (userNum == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (userNum == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            else if (userNum > 2)
+            {
+                Console.WriteLine("A lot!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
