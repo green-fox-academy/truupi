62715c1469dc48c88cafb4c74a2ce486a22cdaf2
Tamas <deaktamas93@gmail.com>
2017-09-12 01:02:24 +0200

    counter configured with a while loop

---
+++ week-02/day-01/print-even_ex24/print-even_ex24/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_even_ex24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 2;
+            Console.WriteLine("Even numbers between 0 to 500\n");
+
+            while (counter <= 500)
+            {
+                Console.WriteLine(counter);
+                counter = counter + 2;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
