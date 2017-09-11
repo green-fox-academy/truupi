f8a0d7400984593a6f01e0ce816408daabee1841
Tamas <deaktamas93@gmail.com>
2017-09-12 00:23:14 +0200

    party indicator basic structure created

---
+++ week-02/day-01/party-indicator_ex21/party-indicator_ex21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace party_indicator_ex21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's see how good the party was! How many girls were there? ");
+            string girls = Console.ReadLine();
+            Console.WriteLine("Good! Now tell me how many boys came? ");
+            string boys = Console.ReadLine();
+
+            int userNum1 = Int32.Parse(girls);
+            int userNum2 = Int32.Parse(boys);
+
+            if (userNum1 > userNum2)
+            {
+                Console.WriteLine(userNum1 + " is the bigger one!");
+            }
+            else
+            {
+                Console.WriteLine(userNum2 + " is the bigger one!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
