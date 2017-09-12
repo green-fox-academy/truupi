3344b8b32c72f88cbc180f82dca242a8d9f46dbf
Tamas <deaktamas93@gmail.com>
2017-09-12 17:17:51 +0200

    (feat: function) factorial working

---
+++ week-02/day-02/ex05_factorio/ex05_factorio/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex05_factorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write here the number for which you want to know its factorial: \n");
+            int input = Int32.Parse(Console.ReadLine());
+            int result = Factorial(input);
+            Console.WriteLine("\nYour result:\n" + result + "\nNice!");
+            Console.ReadLine();
+        }
+
+        static int Factorial(int input)
+        {
+               if (input <= 1)
+                   return 1;
+               return input * Factorial(input--);
+        }
+    }
+}
