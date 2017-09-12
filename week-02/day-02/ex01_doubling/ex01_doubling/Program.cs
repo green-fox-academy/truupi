dd77f595bf337aacfa8e0e66dbc5186a1bdc8800
Tamas <deaktamas93@gmail.com>
2017-09-12 13:26:05 +0200

    feat(added) doubling function

---
+++ week-02/day-02/ex01_doubling/ex01_doubling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01_doubling
+{
+    class Program
+    {
+        public static int Doubling(int input1)
+        {
+            int result = input1 * 2;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int ak = 123;
+            int result = Doubling(ak);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
