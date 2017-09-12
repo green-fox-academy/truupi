0d3c0e93ee7f75f1965424c0027a6af88cab8b2a
Tamas <deaktamas93@gmail.com>
2017-09-12 13:31:42 +0200

    (feat) added: greet function

---
+++ week-02/day-02/ex02_greet/ex02_greet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex02_greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string al = "Greenfox";
+            string result = Greet(al);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+
+        static string Greet(string in1)
+        {
+            string result = "Greetings, dear " + in1 + "!";
+            return result;
+        }
+    }
+}
