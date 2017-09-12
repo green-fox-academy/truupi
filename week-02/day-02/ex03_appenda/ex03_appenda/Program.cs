e51ba99399be6539b2ed4a194ab1534136f58407
Tamas <deaktamas93@gmail.com>
2017-09-12 13:36:40 +0200

    (feat) added: adding 'a' to word's end funtion

---
+++ week-02/day-02/ex03_appenda/ex03_appenda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex03_appenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "kuty";
+            string result = appendA(am);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static string appendA(string in1)
+        {
+            string result = in1 + "a";
+            return result;
+        }
+    }
+
+}
---
+++ week-02/day-02/ex03_appenda/ex03_appenda/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ex03_appenda")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ex03_appenda")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9e3f06e8-fcec-44b8-a5ea-6dc602055ff1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
