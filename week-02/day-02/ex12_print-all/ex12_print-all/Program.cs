41f9db5ecbea1f02264dc63309d8e3bfd6171076
Tamas <deaktamas93@gmail.com>
2017-09-13 12:15:22 +0200

    array created and values printed using foreach in loop

---
+++ week-02/day-02/ex12_print-all/ex12_print-all/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex12_print_all
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] af = { 4, 5, 6, 7 };
+
+            foreach (int element in af)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+        }
+    }
+}
