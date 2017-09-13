be1f875ba0fb15e70be96deb7854e24744b2914c
Tamas <deaktamas93@gmail.com>
2017-09-13 16:36:27 +0200

    (feat) added: stringbuilder inserts text

---
+++ week-02/day-03/Ex3/Ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder qoute = new StringBuilder("Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.");
+            Console.WriteLine(qoute.Insert(21, "always takes longer than "));
+            Console.ReadLine();
+        }
+    }
+}
