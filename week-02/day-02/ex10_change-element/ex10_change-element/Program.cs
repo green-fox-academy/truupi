769e5e00fba10e8068ac215f61a7500e30547ecc
Tamas <deaktamas93@gmail.com>
2017-09-13 12:02:22 +0200

    array created then 4th element successfully changed and printed

---
+++ week-02/day-02/ex10_change-element/ex10_change-element/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex10_change_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] s = { 1, 2, 3, 8, 5, 6 };
+            s[3] = 4;
+            Console.WriteLine(s[3]);
+            Console.ReadLine();
+
+        }
+    }
+}
