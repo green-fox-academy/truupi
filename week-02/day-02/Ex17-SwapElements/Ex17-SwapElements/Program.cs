a3bbd4328f3cdf3f3e788c3d1fed6ff2a8a6b57f
Tamas <deaktamas93@gmail.com>
2017-09-13 15:13:03 +0200

    (feat) added: array elements swapped and written down with foreach

---
+++ week-02/day-02/Ex17-SwapElements/Ex17-SwapElements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex17_SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] abc = { "first", "second", "third" };
+            
+            string temp = abc[0];
+            abc[0] = abc[2];
+            abc[2] = temp;
+
+            foreach (string item in abc)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
