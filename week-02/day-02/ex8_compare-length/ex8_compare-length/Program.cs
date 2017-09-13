c17e3c48fdccd669aeb0e086095f61733417bf34
Tamas <deaktamas93@gmail.com>
2017-09-13 11:54:50 +0200

    (feat) added: array comparison working

---
+++ week-02/day-02/ex8_compare-length/ex8_compare-length/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex8_compare_length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p1 = { 1, 2, 3 };
+            int[] p2 = { 4, 5 };
+
+            if (p1.Length < p2.Length)
+            {
+                Console.WriteLine("P2 has more elements");
+            }
+            else Console.WriteLine("P1 has more elements");
+
+            Console.ReadLine();
+        }
+    }
+}
