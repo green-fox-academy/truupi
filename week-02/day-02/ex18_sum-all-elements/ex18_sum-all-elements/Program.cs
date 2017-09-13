0fc846f2802be36a4f388b5a28b2ad5368c3ca64
Tamas <deaktamas93@gmail.com>
2017-09-13 14:18:33 +0200

    array created and values printed using string manipulation

---
+++ week-02/day-02/ex18_sum-all-elements/ex18_sum-all-elements/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex18_sum_all_elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ai = { 3, 4, 5, 6, 7 };
+            Console.WriteLine(ai.Sum());
+            Console.ReadLine();
+        }
+    }
+}
