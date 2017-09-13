c75328a30bd1b5364b72a17cb080c95b77b39d28
Tamas <deaktamas93@gmail.com>
2017-09-13 15:55:52 +0200

    (feat) added: multiple functions repairing the broken string link

---
+++ week-02/day-03/Ex2/Ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UrlFixer());
+            Console.ReadLine();
+        }
+
+        static string UrlFixer()
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+            url = UrlSwap(url);
+            url = UrlColon(url);
+            return url;
+        }
+        
+        static string UrlSwap(string input)
+        {
+            string swap = input.Replace("bots", "odds");
+            return swap;
+        }
+        
+        static string UrlColon(string input)
+        {
+            string modified = input.Insert(5, ":");
+            return modified;
+        }
+    }
+}
