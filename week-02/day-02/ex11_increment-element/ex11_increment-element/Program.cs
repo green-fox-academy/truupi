ab574e47ff1158829161bf36e4cfaadc062eacc2
Tamas <deaktamas93@gmail.com>
2017-09-13 12:05:18 +0200

    array created then third element successfully incremented and printed

---
+++ week-02/day-02/ex11_increment-element/ex11_increment-element/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex11_increment_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = { 1, 2, 3, 4, 5 };
+            t[2]++;
+            Console.WriteLine(t[2]);
+            Console.ReadLine();
+        }
+    }
+}
