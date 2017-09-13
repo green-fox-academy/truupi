7725a0b8474a81aef5fe51e1fc683fbf09ad5937
Tamas <deaktamas93@gmail.com>
2017-09-13 23:37:00 +0200

    (feat) added: elements swapped and written with foreach

---
+++ week-02/day-03/Ex9-Candyshop/Ex9-Candyshop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex9_Candyshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            list[1] = "Croissant";
+            list[3] = "Ice cream";
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
