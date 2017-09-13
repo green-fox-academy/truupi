6d283f3bb4fdae2bc9874f26f7dc573994d98792
Tamas <deaktamas93@gmail.com>
2017-09-13 13:30:23 +0200

    (feat) added: function dinamically creates array, writes it down in matrix and fills up array values

---
+++ week-02/day-02/ex13_matrix/ex13_matrix/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex13_matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] table = DrawMatrix(17);
+            Console.ReadLine();
+        }
+
+        static int[,] DrawMatrix(int size)
+        {
+            int[,] table = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == j)
+                    {
+                        table[i, j] = 1;
+                        Console.Write(1);
+                    }
+                    else
+                    {
+                        Console.Write(0);
+                    }
+                }
+                Console.WriteLine();
+            }
+            return table;
+        }
+    }
+}
