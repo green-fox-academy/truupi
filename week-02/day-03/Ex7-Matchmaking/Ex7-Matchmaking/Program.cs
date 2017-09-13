8e49ed67311075b34e990593975a005224c3925f
Tamas <deaktamas93@gmail.com>
2017-09-13 17:58:09 +0200

    initial commit

---
+++ week-02/day-03/Ex7-Matchmaking/Ex7-Matchmaking/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7_Matchmaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+            var order = new List<string>();
+
+            // Join the two lists by matching one girl with one boy in the order list
+            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+            Console.WriteLine(order);
+        }
+    }
+}
