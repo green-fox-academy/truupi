ff0416a19531d30dfd2ab6bbb98b3470a064b6df
Tamas <deaktamas93@gmail.com>
2017-09-13 15:23:05 +0200

    (feat) added: array elements doubled with for loop

---
+++ week-02/day-02/Ex14-DoubleItems/Ex14-DoubleItems/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex14_DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ag = { 3, 4, 5, 6, 7 };
+
+            for (int i = 0; i < ag.Length; i++)
+            {
+                Console.WriteLine(ag[i] * 2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
