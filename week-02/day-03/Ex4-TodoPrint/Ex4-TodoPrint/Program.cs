3b1525f31b2327f546323e622309ee7c04ff674a
Tamas <deaktamas93@gmail.com>
2017-09-13 17:08:17 +0200

    (feat) added: todo list added to together with a function, converted to string and written in Main

---
+++ week-02/day-03/Ex4-TodoPrint/Ex4-TodoPrint/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4_TodoPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Adder());
+            Console.ReadLine();
+        }
+
+        static string Adder()
+        {
+            var todoText = new StringBuilder(" - Buy milk\n");
+            todoText.Insert(0, "My todo:\n");
+            int k = todoText.Length;
+            todoText.Insert(k, " - Download games\n");
+            string j = "".PadLeft(4);
+            int l = todoText.Length;
+            todoText.Insert(l, j + "-Diablo");
+            return todoText.ToString();
+        }
+    }
+}
