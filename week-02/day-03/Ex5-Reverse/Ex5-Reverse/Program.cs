35bf029656206d01aecf19c30397ea75b5981874
Tamas <deaktamas93@gmail.com>
2017-09-13 17:13:43 +0200

    (feat) added: reverser function is able to reverse the letters of any given input

---
+++ week-02/day-03/Ex5-Reverse/Ex5-Reverse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+            Console.WriteLine(Reverser(reversed));
+            Console.ReadLine();
+        }
+
+        static string Reverser(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
