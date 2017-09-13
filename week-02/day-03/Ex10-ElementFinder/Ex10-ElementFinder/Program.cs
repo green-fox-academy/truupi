a07b5e407b0583f8e49a782f8d3d146cc5d6acd5
Tamas <deaktamas93@gmail.com>
2017-09-14 01:22:58 +0200

    (feat) added: function checks whether List contains userInput or not

---
+++ week-02/day-03/Ex10-ElementFinder/Ex10-ElementFinder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10_ElementFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to see if it exists in our list or not: \n");
+            int input = int.Parse(Console.ReadLine());
+            Check(input);
+            Console.ReadLine();
+        }
+
+        static string Check(int input)
+        {
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+            int contain = 0;
+            foreach (var item in List)
+            {
+                if (item == input)
+                {
+                    contain = 1;
+                }
+            }
+            if (contain == 1)
+            {
+                Console.WriteLine("\nHuurray! You have a match!");
+            }
+            else
+            {
+                Console.WriteLine("\nNooo! No matches this time!");
+            }
+            return input.ToString();
+        }
+    }
+}
