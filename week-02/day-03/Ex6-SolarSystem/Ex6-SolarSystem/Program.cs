69a7565c0d183ad17cddb51c59b99a4fa9fe2fd1
Tamas <deaktamas93@gmail.com>
2017-09-13 17:51:55 +0200

    (feat) added: function adds input into the list as the 5th element

--- week-02/day-03/Ex6-SolarSystem/Ex6-SolarSystem/Program.cs
+++ week-02/day-03/Ex6-SolarSystem/Ex6-SolarSystem/Program.cs
@@ -10,6 +10,19 @@ namespace Ex6_SolarSystem
     {
         static void Main(string[] args)
         {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus",
+            "Neptune" };
+            List(planetList);
+            Console.ReadLine();
+        }
+
+        static void List(List<string> input)
+        {
+            input.Insert(5, "Saturn");
+            foreach (var item in input)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
