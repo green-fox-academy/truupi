9b2163f414ee5bcf07c9497bd4cae6869e2946d2
Tamas <deaktamas93@gmail.com>
2017-09-13 15:34:28 +0200

    (feat) added: replacer function working

---
+++ week-02/day-03/Ex1/Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Ex());
+            Console.ReadLine();
+        }
+
+        static string Ex()
+        {
+            string example = "In a dishwasher far far away";
+            string result = example.Replace("dishwasher", "galaxy");
+            return result;
+        }
+    }
+}
