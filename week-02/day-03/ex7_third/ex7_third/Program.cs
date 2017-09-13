d9b0b68e3f9dde88a624d324ff3279258386e51a
Tamas <deaktamas93@gmail.com>
2017-09-13 11:48:27 +0200

    (feat: added) array element written out

---
+++ week-02/day-03/ex7_third/ex7_third/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex7_third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] q = { 4, 5, 6, 7 };
+
+            Console.WriteLine(q[2]);
+            Console.ReadLine();
+        }
+    }
+}
