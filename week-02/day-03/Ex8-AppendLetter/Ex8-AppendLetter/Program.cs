460839b9e2cae07670039b163b0ce3e3c4e10a91
Tamas <deaktamas93@gmail.com>
2017-09-13 23:23:21 +0200

    (feat) added: letter a added to the end of each list element

---
+++ week-02/day-03/Ex8-AppendLetter/Ex8-AppendLetter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex8_AppendLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+
+            foreach (var item in far)
+            {
+                Console.WriteLine(item + "a");
+            }
+            Console.ReadLine();
+        }
+    }
+}
