b6ddb6d8edacfafa6aaea1380fd44868193b68de
Tamas <deaktamas93@gmail.com>
2017-09-14 10:35:59 +0200

    feat(program): funtion computes two userinputs by multiplying each and gives back summed up result with user friendly environment

--- week-02/day-01/Ex16-AnimalsAndLegs/Ex16-AnimalsAndLegs/Program.cs
+++ week-02/day-01/Ex16-AnimalsAndLegs/Ex16-AnimalsAndLegs/Program.cs
@@ -10,6 +10,19 @@ namespace Ex16_AnimalsAndLegs
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please type here the number of CHICKENS the farmer should have:\n");
+            int userInput = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nPlease type here the number of PIGS the farmer should have:\n");
+            int userInput2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nAll the animals on the farm have " + Count(userInput, userInput2) + " legs altogether.");
+            Console.ReadLine();
+        }
+
+        static int Count(int input, int input2)
+        {
+            int input3 = (input * 2) + (input2 * 4);
+            return input3;
         }
     }
 }
