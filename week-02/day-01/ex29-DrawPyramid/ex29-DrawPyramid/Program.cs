12ffdc0019f5da13f407e2235262371eb2757545
Tamas <deaktamas93@gmail.com>
2017-09-14 19:09:21 +0200

    changed

--- week-02/day-01/ex29-DrawPyramid/ex29-DrawPyramid/Program.cs
+++ week-02/day-01/ex29-DrawPyramid/ex29-DrawPyramid/Program.cs
@@ -10,12 +10,11 @@ namespace ex29_DrawPyramid
     {
         static void Main(string[] args)
         {
-
             int numberoflayer = Convert.ToInt32(Console.ReadLine()), Space, Number;
             for (int i = 1; i <= numberoflayer; i++)
             {
                 for (Space = 1; Space <= (numberoflayer - i); Space++)
-                    Console.Write(" ");
+                    Console.Write("");
                 for (Number = 1; Number <= i; Number++)
                     Console.Write('*');
                 for (Number = (i - 1); Number >= 1; Number--)
