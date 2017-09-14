16f362cbe436b16883b6ef48f5b1ba18855684f8
Tamas <deaktamas93@gmail.com>
2017-09-14 11:41:11 +0200

    feat(program): basic operations created, functions stacked up, calculator functions need to be declared to work

--- week-02/day-01/ex17-AverageOfInput/ex17-AverageOfInput/Program.cs
+++ week-02/day-01/ex17-AverageOfInput/ex17-AverageOfInput/Program.cs
@@ -10,6 +10,32 @@ namespace ex17_AverageOfInput
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please type here five different numbers in a row. Please separate them by pressing <Enter> between them:\n");
+            Console.WriteLine(Avg(CreateArray()) + Sum(CreateArray()));
+            Console.ReadLine();
+        }
+
+        static int[] CreateArray()
+        {
+            int[] userInputsArray = new int[5];
+            for (int i = 0; i < userInputsArray.Length; i++)
+            {
+                userInputsArray[i] = int.Parse(Console.ReadLine());
+            }
+            return userInputsArray;
+        }
+
+        static int Avg(int[] input)
+        {
+
+            int result = ;
+            return result;
+        }
+
+        static int Sum(int[] input)
+        {
+            int result = input.Average;
+            return result;
         }
     }
 }
