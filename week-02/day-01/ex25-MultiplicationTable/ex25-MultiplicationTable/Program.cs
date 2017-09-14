d8f824e1b6799226cdae7652ceb6cd4f151aa709
Tamas <deaktamas93@gmail.com>
2017-09-14 16:22:18 +0200

    feat(multiplication): userinput multiplied successfully with two for loops

---
+++ week-02/day-01/ex25-MultiplicationTable/ex25-MultiplicationTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex25_MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < 10+1; i++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    int calculation = i * number;
+                    Console.WriteLine(i + " * " + number + " = " + (calculation));
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
