5220298df1086622d9977292942042124f71556d
Tamas <deaktamas93@gmail.com>
2017-09-15 11:35:52 +0200

    feat(PalindromeSearcher): initial structure

---
+++ week-02/day-05/PalindromeSearcher/PalindromeSearcher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeSearcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        
+        static List[] PalindromeSearcher(string input1)
+        {
+
+        }
+
+        static void ListCreator()
+        {
+
+        }
+
+        static void IsPalindrome()
+        {
+            
+        }
+    }
+}
