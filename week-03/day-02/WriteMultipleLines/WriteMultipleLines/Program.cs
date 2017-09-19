48c82afec5455f56a0d82fe59c2305d97777b66b
Tamas <deaktamas93@gmail.com>
2017-09-19 13:34:49 +0200

    feat(WriteMultipleLines) added: function successfully creates a file, writes input string in it multiple times based on a for loop

---
+++ week-03/day-02/WriteMultipleLines/WriteMultipleLines/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteMultipleLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Writer();
+        }
+        
+        static void Writer()
+        {
+            string path = @"../../my-file.txt";
+            string word = "apple";
+            int number = 5;
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    for (int i = 0; i < number; i++)
+                    {
+                        writer.WriteLine(word);
+                    }
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Can not write the file!");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
