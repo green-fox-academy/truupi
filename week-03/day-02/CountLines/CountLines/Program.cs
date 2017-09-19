f6e4d1245834721008b2db24034961745f20ca39
Tamas <deaktamas93@gmail.com>
2017-09-19 13:07:53 +0200

    feat(CountLines) added: correctly read line number if existing entered, otherwise returns an error msg

---
+++ week-03/day-02/CountLines/CountLines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"../../../../encoded-lines.txt";
+            try
+            {
+                var countline = File.ReadLines(path).Count();
+                Console.WriteLine(countline);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("File not found!");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
