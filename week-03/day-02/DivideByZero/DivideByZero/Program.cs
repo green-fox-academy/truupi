474a67364f9a1f570d613c6d15a92b87346a3e2c
Tamas <deaktamas93@gmail.com>
2017-09-19 12:15:43 +0200

    feat(dividebyzero): if dividing by zero, an error message appears

---
+++ week-03/day-02/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the number that you want 10 divide with:");
+                int userInput = int.Parse(Console.ReadLine());
+                int result = 10 / userInput;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException error)
+            {
+                Console.WriteLine("Cannot divide by zero!");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("An unknown error happened!");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
