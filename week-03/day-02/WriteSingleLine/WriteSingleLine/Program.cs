f21aa532cb464de9845efb2195d06bcd31f14504
Tamas <deaktamas93@gmail.com>
2017-09-19 13:15:43 +0200

    feat(WriteSingleLine) added: correctly writes input line into file if it can create it, otherwise return error msg

---
+++ week-03/day-02/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"my-file.txt";
+            try
+            {
+                using (var writer = new StreamWriter(path))
+                {
+                    writer.WriteLine("Deak Tamas");
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Unable to write file: my-file.txt");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
