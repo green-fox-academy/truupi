ecf13d7f07b2793a31cfbe06ba4804db50ef94d7
Tamas <deaktamas93@gmail.com>
2017-09-20 13:48:53 +0200

    feature(program): excercise done

---
+++ week-03/day-03/Ex6-CenteredSquare/Ex6-CenteredSquare/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Ex6_CenteredSquare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var foxDraw = new FoxDraw(canvas);
+
+            foxDraw.BackgroundColor(Colors.Black);
+            foxDraw.StrokeColor(Colors.Green);
+            double halfwidth = canvas.Width / 2;
+            double halfheigth = canvas.Height / 2;
+            double side = 50;
+            foxDraw.DrawLine(halfwidth - side, halfheigth - side, halfwidth + side, halfheigth - side);
+            foxDraw.DrawLine(halfwidth + side, halfheigth - side, halfwidth + side, halfheigth + side);
+            foxDraw.DrawLine(halfwidth + side, halfheigth + side, halfwidth - side, halfheigth + side);
+            foxDraw.DrawLine(halfwidth - side, halfheigth + side, halfwidth - side, halfheigth - side);
+        }
+    }
+}
