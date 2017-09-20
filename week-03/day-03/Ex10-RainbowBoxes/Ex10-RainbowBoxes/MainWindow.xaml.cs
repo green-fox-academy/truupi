25d21ee9722c885c731602d19f49909bbc44022d
Tamas <deaktamas93@gmail.com>
2017-09-20 15:29:22 +0200

    feature(program): excercise done

---
+++ week-03/day-03/Ex10-RainbowBoxes/Ex10-RainbowBoxes/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Ex10_RainbowBoxes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Random rnd = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            for (int i = 300; i > 0; i -= 10)
+            {
+                RectangleDrawer(i, foxDraw, RandomColorGenerator());
+            }
+            
+        }
+
+        public Color RandomColorGenerator()
+        {
+            var randomColor = Color.FromArgb((byte)rnd.Next(100), (byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256));
+            return randomColor;
+        }
+
+        public void RectangleDrawer(double size, FoxDraw foxDraw, Color color)
+        {
+            double halfwidth = canvas.Width / 2;
+            double halfheigth = canvas.Height / 2;
+            foxDraw.FillColor(color);
+            foxDraw.DrawRectangle(halfwidth - size / 2, halfheigth - size / 2, size, size);
+        }
+    }
+}
