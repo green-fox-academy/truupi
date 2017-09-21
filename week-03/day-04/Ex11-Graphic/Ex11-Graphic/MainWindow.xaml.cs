54f8711e8f1a82ad589189b108569e87267a460c
Tamas <deaktamas93@gmail.com>
2017-09-21 18:20:10 +0200

    feat(program): basic structure done

--- week-03/day-04/Ex11-Graphic/Ex11-Graphic/MainWindow.xaml.cs
+++ week-03/day-04/Ex11-Graphic/Ex11-Graphic/MainWindow.xaml.cs
@@ -25,13 +25,40 @@ namespace Ex11_Graphic
         {
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
+            Basic(foxDraw);
+            int x = Convert.ToInt32(canvas.Width / 3);
+            int y = Convert.ToInt32(canvas.Height / 3);
+            int n = 1;
+            Smaller(foxDraw, x, y, n);
+            
+        }
 
+        public void Basic(FoxDraw foxDraw)
+        {
             foxDraw.FillColor(Colors.Yellow);
             foxDraw.DrawRectangle(0, 0, canvas.Width, canvas.Height);
             foxDraw.DrawLine(0, canvas.Height / 3, canvas.Width, canvas.Height / 3);
             foxDraw.DrawLine(0, canvas.Height / 3 * 2, canvas.Width, canvas.Height / 3 * 2);
             foxDraw.DrawLine(canvas.Width / 3, 0, canvas.Width / 3, canvas.Height);
             foxDraw.DrawLine(canvas.Width / 3 * 2, 0, canvas.Width / 3 * 2, canvas.Height);
+        }
+
+        public int Smaller(FoxDraw foxDraw, int x, int y, int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                foxDraw.FillColor(Colors.Yellow);
+                foxDraw.DrawRectangle(0, 0, canvas.Height / 3, canvas.Height / 3);
+                foxDraw.DrawLine(0, x / 3, y, y / 3);
+                foxDraw.DrawLine(0, x / 3 * 2, x, y / 3 * 2);
+                foxDraw.DrawLine(x / 3, 0, x / 3, y);
+                foxDraw.DrawLine(x / 3 * 2, 0, x / 3 * 2, y);
+             return Smaller(foxDraw, x / 3, y / 3, n - 1);
+            }
         }
     }
 }
