462c418c2cd55780ef4d5b9e7139f6a718181071
Tamas <deaktamas93@gmail.com>
2017-09-21 16:50:11 +0200

    feat(program): excercise done

---
+++ week-03/day-04/Ex08-String2/Ex08-String2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex08_String2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "xXoxoXoOXoxoXOXXoxxx";
+            Console.WriteLine(SwapXtoZero(word));
+            Console.ReadLine();
+        }
+
+        private static string SwapXtoZero(string word)
+        {
+            string result = "";
+            if (word.Length == 0)
+            {
+                return "";
+            }
+            if (word.Substring(word.Length - 1, 1) == "x")
+            {
+                result = "";
+                return SwapXtoZero(word.Substring(0, word.Length - 1)) + result;
+            }
+            return SwapXtoZero(word.Substring(0, word.Length - 1)) + word.Substring(word.Length - 1, 1);
+        }
+    }
+}
---
+++ week-03/day-04/Ex09-String3/Ex09-String3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex09_String3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "xXoxoXoOXoxoXOXXoxxx";
+            Console.WriteLine(SwapXtoZero(word));
+            Console.ReadLine();
+        }
+
+        private static string SwapXtoZero(string word)
+        {
+            if (word.Length == 0)
+            {
+                return "";
+            }
+            return SwapXtoZero(word.Substring(0, word.Length - 1)) + (word.Substring(word.Length - 1, 1)) + "*";
+        }
+    }
+}
