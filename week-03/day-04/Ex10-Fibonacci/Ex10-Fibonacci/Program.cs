d44071abc6ba0da28d780766174f8b955ea2b2be
Tamas <deaktamas93@gmail.com>
2017-09-21 17:06:54 +0200

    feat(program): excercise done

---
+++ week-03/day-04/Ex10-Fibonacci/Ex10-Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciVarations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 9;
+            Console.WriteLine(GetNthFibonacci(number));
+            Console.ReadLine();
+        }
+
+        private static double GetNthFibonacci(double number)
+        {
+            double nthFibonacci = 0;
+            if (number == 2)
+            {
+                return 1;
+            }
+            if (number <= 2)
+            {
+                return nthFibonacci;
+            }
+            return GetNthFibonacci(number - 1) + GetNthFibonacci(number - 2);
+        }
+    }
+}
