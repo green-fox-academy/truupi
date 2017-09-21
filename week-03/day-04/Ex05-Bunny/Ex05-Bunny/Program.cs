fdb524ebeceee12089bcebdfd7e19a233baf6dcb
Tamas <deaktamas93@gmail.com>
2017-09-21 10:44:36 +0200

    feat(program): refactoring

---
+++ week-03/day-04/Ex05-Bunny/Ex05-Bunny/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05_Bunny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int BunnyNum = 13;
+            Console.WriteLine(Recursive(BunnyNum));
+            Console.ReadLine();
+        }
+
+        static int Recursive(int BunnyNum)
+        {
+            int ears = 2;
+            if (BunnyNum == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return ears + Recursive(BunnyNum - 1);
+            }
+        }
+    }
+}
