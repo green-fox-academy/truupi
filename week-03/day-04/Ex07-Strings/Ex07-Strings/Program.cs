2cb0be9daa181b4793cc6732ac71fdcdea7a06ff
Tamas <deaktamas93@gmail.com>
2017-09-21 16:42:02 +0200

    feat(program): excercise done

---
+++ week-03/day-04/Ex07-Strings/Ex07-Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "xXXxXxxXoXoXXxxx";
+            Console.WriteLine(SwapXtoY(word));
+            Console.ReadLine();
+        }
+
+        static string SwapXtoY(string word)
+        {
+            string result = "";
+            if (word.Length == 0)
+            {
+                return "";
+            }
+            if (word.Substring(word.Length - 1, 1) == "x")
+            {
+                result = "y";
+                return SwapXtoY(word.Substring(0, word.Length - 1)) + result;
+            }
+            return SwapXtoY(word.Substring(0, word.Length - 1)) + word.Substring(word.Length - 1, 1);
+        }
+    }
+}
