55a1fc4556b7039cb83d8b265a034c2f1eca3d03
Tamas <deaktamas93@gmail.com>
2017-09-22 11:30:59 +0200

    initial commit

--- week-03/day-05/Ex01-Triangles/Ex01-Triangles/MainWindow.xaml.cs
+++ week-03/day-05/Ex01-Triangles/Ex01-Triangles/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GreenFox;
 
 namespace Ex01_Triangles
 {
@@ -23,6 +24,28 @@ namespace Ex01_Triangles
         public MainWindow()
         {
             InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.FillColor(Colors.Yellow);
+            foxDraw.DrawRectangle(0, 0, canvas.Width, canvas.Height);
+            Drawer(foxDraw, 0, 0, canvas.Width, canvas.Height, 5);
+        }
+
+        public void Drawer(FoxDraw foxDraw, double startx, double starty, double endx, double endy, int depth)
+        {
+            foxDraw.DrawLine(startx + (endx - startx) / 3, starty, startx + (endx - startx) / 3, endy);
+            foxDraw.DrawLine(startx + (endx - startx) / 1.5, starty, startx + (endx - startx) / 1.5, endy);
+            foxDraw.DrawLine(startx, starty + (endy - starty) / 3, endx, starty + (endy - starty) / 3);
+            foxDraw.DrawLine(startx, starty + (endy - starty) / 1.5, endx, starty + (endy - starty) / 1.5);
+
+            depth--;
+
+            if (depth > 0)
+            {
+                Drawer(foxDraw, startx + (endx - startx) / 3, starty, startx + (endx - startx) / 1.5, starty + (endy - starty) / 3, depth);
+                Drawer(foxDraw, startx + (endx - startx) / 3, starty + (endy - starty) / 1.5, startx + (endx - startx) / 1.5, endy, depth);
+                Drawer(foxDraw, startx, starty + (endy - starty) / 3, startx + (endx - startx) / 3, starty + (endy - starty) / 1.5, depth);
+                Drawer(foxDraw, startx + (endx - startx) / 1.5, starty + (endy - starty) / 3, endx, starty + (endy - starty) / 1.5, depth);
+            }
         }
     }
 }
