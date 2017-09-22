c1b03d6ed103b7c2a700f616781892a18c59d329
Tamas <deaktamas93@gmail.com>
2017-09-22 09:10:38 +0200

    project (feature): program functions with error messages and console clears for problems

--- ExercisesMadeForEntertainment/RecurseDecToBinConverter/RecurseDecToBinConverter/Program.cs
+++ ExercisesMadeForEntertainment/RecurseDecToBinConverter/RecurseDecToBinConverter/Program.cs
@@ -82,7 +82,6 @@ namespace RecurseDecToBinConverter
                 else if (answer == "n")
                 {
                     Closing();
-                    
                 }
                 TrueOrFalse();
                 break;
