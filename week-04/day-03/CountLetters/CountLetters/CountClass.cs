19aab38bc24d3bea748d761896a0c3e53156da49
Tamas <deaktamas93@gmail.com>
2017-09-27 16:42:14 +0200

    feat(program): StringToDict funtion counts letters in input string and returns a dictionary with chars, test passed and working

---
+++ week-04/day-03/CountLetters/CountLetters/CountClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetters
+{
+    public class CountClass
+    {
+        public Dictionary<char, int> StringToDict(string word)
+        {
+            Dictionary<char, int> resultDict = new Dictionary<char, int>();
+
+            if (String.IsNullOrEmpty(word))
+            {
+                return resultDict;
+            }
+            foreach (char letter in word)
+            {
+                if (!resultDict.ContainsKey(letter))
+                {
+                    resultDict.Add(letter, 1);
+                }
+                else
+                {
+                    resultDict[letter]++;
+                }
+            }
+            return resultDict;
+        }
+    }
+}
---
+++ week-04/day-03/CountLetters/CountLettersTest/CountClassTest.cs
@@ -0,0 +1,30 @@
+﻿using CountLetters;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLettersTest
+{
+    [TestFixture]
+
+    public class CountClassTest
+    {
+        CountClass testing = new CountClass();
+
+        [Test]
+        public void IsItRight()
+        {
+            string input = "alma";
+            Dictionary<char, int> testDict = new Dictionary<char, int>()
+            {
+                { 'a', 2 },
+                { 'l', 1 },
+                { 'm', 1 }
+            };
+            Assert.AreEqual(testDict, testing.StringToDict(input));
+        }
+    }
+}
