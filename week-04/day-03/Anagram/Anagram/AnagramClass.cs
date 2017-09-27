fa6ffd3aa71a0e680bc90d7c93dec89377212d8e
Tamas <deaktamas93@gmail.com>
2017-09-27 16:07:51 +0200

    refactoring(Program): refactoring level 100

--- week-04/day-03/Anagram/Anagram/AnagramClass.cs
+++ week-04/day-03/Anagram/Anagram/AnagramClass.cs
@@ -10,31 +10,14 @@ namespace Anagram
     {
         public bool IsAnagram(string word, string word2)
         {
-            bool isitanagram = true;
-            char[] char1 = SortArray(ConvertString(word));
-            char[] char2 = SortArray(ConvertString(word2));
-
-            for (int i = 0; i < char1.Length; i++)
-            {
-                if (char1[i] != char2[i])
-                {
-                    isitanagram = false;
-                    break;
-                }
-            }
-            return isitanagram;
+            return StringConvert(word) == StringConvert(word2);
         }
 
-        private char[] ConvertString(string inputString)
+        private string StringConvert(string word)
         {
-            char[] char1 = inputString.ToCharArray();
-            return char1;
-        }
-
-        private char[] SortArray(char[] inputArray)
-        {
-            Array.Sort(inputArray);
-            return inputArray;
+            char[] array = word.ToArray();
+            Array.Sort(array);
+            return Convert.ToString(array);
         }
     }
 }
