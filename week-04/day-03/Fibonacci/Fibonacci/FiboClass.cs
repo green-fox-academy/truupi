45e2f2374314b1ecddccc54620f04f97c8185fe1
Tamas <deaktamas93@gmail.com>
2017-09-27 17:04:52 +0200

    feat(program): Fibonacci function with recursion working and test passed

---
+++ week-04/day-03/Fibonacci/Fibonacci/FiboClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class FiboClass
+    {
+        public double GetNthFibonacci(double number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            if (number <= 2)
+            {
+                return 1;
+            }
+            return GetNthFibonacci(number - 1) + GetNthFibonacci(number - 2);
+        }
+    }
+}
---
+++ week-04/day-03/Fibonacci/FibonacciTest/FiboClassTest.cs
@@ -0,0 +1,24 @@
+﻿using Fibonacci;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciTest
+{
+    [TestFixture]
+    public class FiboClassTest
+    {
+        FiboClass fibi = new FiboClass();
+
+        [Test]
+        public void TestingFiboIndex()
+        {
+            double input = 8;
+            int output = 21;
+            Assert.AreEqual(output, fibi.GetNthFibonacci(input));
+        }
+    }
+}
