30bb35bc25d4ccf32e48b13823f7f6bb25ff6850
Tamas <deaktamas93@gmail.com>
2017-09-27 11:30:13 +0200

    feature(Program): AppleTest class successfully tests method without fail

---
+++ week-04/day-03/Apple/AppleTest/AppleClassTest.cs
@@ -0,0 +1,24 @@
+﻿using Apple;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleTest
+{
+    [TestFixture]
+
+    public class AppleClassTest
+    {
+        AppleClass appleclass = new AppleClass();
+
+        [Test]
+        public void Test_Apple_ReturnsApple()
+        {
+            string apple = "apple";
+            Assert.AreEqual(apple, appleclass.GetApple());
+        }
+    }
+}
