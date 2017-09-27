57befeecd7fb81be42088627a4342f3175845f66
Tamas <deaktamas93@gmail.com>
2017-09-27 15:04:55 +0200

    feature(Program): properly working IsAnagram function added and tested to pass

---
+++ week-04/day-03/Anagram/Anagram/AnagramClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class AnagramClass
+    {
+        public bool IsAnagram(string word, string word2)
+        {
+            bool isitanagram = true;
+            char[] char1 = SortArray(ConvertString(word));
+            char[] char2 = SortArray(ConvertString(word2));
+
+            for (int i = 0; i < char1.Length; i++)
+            {
+                if (char1[i] != char2[i])
+                {
+                    isitanagram = false;
+                    break;
+                }
+            }
+            return isitanagram;
+        }
+
+        private char[] ConvertString(string inputString)
+        {
+            char[] char1 = inputString.ToCharArray();
+            return char1;
+        }
+
+        private char[] SortArray(char[] inputArray)
+        {
+            Array.Sort(inputArray);
+            return inputArray;
+        }
+    }
+}
---
+++ week-04/day-03/Anagram/AnagramTest/AnagramClassTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anagram;
+using NUnit.Framework;
+
+namespace AnagramTest
+{
+    [TestFixture]
+
+    public class AnagramClassTest
+    {
+        AnagramClass anie = new AnagramClass();
+
+        [Test]
+        public void AnagramTester()
+        {
+            string word = "bubika";
+            string word2 = "abikub";
+            bool result = true;
+            Assert.AreEqual(result, anie.IsAnagram(word, word2));
+        }
+    }
+}
