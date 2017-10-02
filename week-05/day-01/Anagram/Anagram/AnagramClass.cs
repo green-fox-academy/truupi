4464df3346578902099a31cc224d38e3506f46a0
Tamas <deaktamas93@gmail.com>
2017-10-02 16:22:28 +0200

    feat(program): AnagramCheck funtion compares input strings

---
+++ week-05/day-01/Anagram/Anagram/AnagramClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class AnagramClass
+    {
+        public bool AnagramCheck (string word1, string word2)
+        {
+            return word1 == word2 ? true : false;
+        }
+    }
+
+}
+
---
+++ week-05/day-01/Anagram/AnagramTesting/AnagramClassTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anagram;
+using NUnit.Framework;
+
+namespace AnagramTesting
+{
+    [TestFixture]
+    public class AnagramClassTest
+    {
+        public AnagramClass anclass = new AnagramClass(); 
+
+        [Test]
+        public void IsStringTheSame()
+        {
+            string word1 = "borso";
+            string word2 = "borso";
+            bool result = true;
+            Assert.AreEqual(result, anclass.AnagramCheck(word1, word2));
+            word2 = "osrob";
+            result = true;
+            Assert.AreEqual(result, anclass.AnagramCheck(word1, word2));
+        }
+
+        //[Test]
+        //public void AreStingsLengthTheSame()
+        //{
+        //    string word1 = "borso";
+        //    string word2 = "kiskunfelegyhaza";
+        //    bool result = false;
+        //    Assert.AreEqual(result, anclass.AnagramCheck(word1, word2));
+        //}
+
+        //[Test]
+        //public void UpperOrSmaller()
+        //{
+        //    string word1 = "borso";
+        //    string word2 = "BORSO";
+        //    bool result = false;
+        //    Assert.AreEqual(result, anclass.AnagramCheck(word1, word2));
+        //}
+
+    }
+}
