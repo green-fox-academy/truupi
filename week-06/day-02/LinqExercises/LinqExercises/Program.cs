7cd8d343580d3f3b9e2789d03b94d009a8b0e00f
Tamas <deaktamas93@gmail.com>
2017-10-18 13:54:26 +0200

    program(feat): exercise 4 done

--- week-06/day-02/LinqExercises/LinqExercises/Program.cs
+++ week-06/day-02/LinqExercises/LinqExercises/Program.cs
@@ -9,10 +9,12 @@ namespace LinqExercises
     class Program
     {
         private static int[] numberArray = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+        private static int[] numberArray2 = new[] { 3, 9, 2, 8, 6, 5 };
+
 
         static void Main(string[] args)
         {
-            foreach (var n in SquareOfEvenNumbers(numberArray))
+            foreach (var n in SquaredIsMore(numberArray2))
             {
                 Console.WriteLine(n);
             }
@@ -32,23 +34,28 @@ namespace LinqExercises
         private static IEnumerable<int> EvenNumbers(int[] numberArray)
         {
             IEnumerable<int> evenNumsWithLambda = numberArray.Where(number => number % 2 == 0);
-            IEnumerable<int> evenNumsQuery = 
+            IEnumerable<int> evenNumsWithQuery = 
                 from number in numberArray
                 where number % 2 == 0
                 select number;
 
-            return evenNumsQuery;
+            return evenNumsWithQuery;
         }
 
         private static IEnumerable<int> SquareOfEvenNumbers(int[] numberArray)
         {
             IEnumerable<int> squareOfEvenNumsWithLambda = numberArray.Where(number => number > 0).Select(number => number * number);
-            IEnumerable<int> squareOfEvenNumsQuery = 
+            IEnumerable<int> squareOfEvenNumsWithQuery = 
                 from number in numberArray
                 where number > 0
                 select number * number;
 
             return squareOfEvenNumsWithLambda;
+        }
+
+        private static IEnumerable<double> SquaredIsMore(int[] numberArray2)
+        {
+            return numberArray2.Select(number => number * number).Where(number => number > 20).Select(number => Math.Sqrt(number));
         }
     }
 }
