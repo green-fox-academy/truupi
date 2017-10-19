7af8245cd4f006bbb6a49f73a9747ce13ab09967
Tamas <deaktamas93@gmail.com>
2017-10-19 17:25:24 +0200

    program(feat): sorting function created

---
+++ week-06/day-04/SortTheArray/SortTheArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintArray(SortArray(new int[] { 1, 2, 3, 4 }));
+            PrintArray(SortArray(new char[] { 'a', 's', 'd', 'f' }));
+            PrintArray(args);
+            Console.ReadLine();
+        }
+
+        public static T[] SortArray<T>(T[] array)
+        {
+            return array.OrderByDescending(x => x).ToArray();
+        }
+
+        public static void PrintArray<T>(T[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
