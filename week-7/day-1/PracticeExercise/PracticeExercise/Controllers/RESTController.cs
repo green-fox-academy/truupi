ee94fd631814bb592deb51c3f159670ad6e0e656
Tamas <deaktamas93@gmail.com>
2017-10-24 14:04:35 +0200

    initial commit and REST Hello User sub exercise done

---
+++ week-7/day-1/PracticeExercise/PracticeExercise/Controllers/RESTController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PracticeExercise.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PracticeExercise.Controllers
+{
+    [Route("api")]  
+    public class RESTController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
+        {
+            return new JsonResult(new Greeting { Id = 0, Content = $"Hello {name}!" });
+        }
+    }
+}
