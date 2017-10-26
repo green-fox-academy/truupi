309a032b86651ebe378f840e3c230a1c3ff094e5
Tamas <deaktamas93@gmail.com>
2017-10-26 14:18:22 +0200

    exercise done

---
+++ week-7/day-3/CounterApp/CounterApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CounterApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CounterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        Counter counter;
+
+        public HomeController(Counter counter)
+        {
+            this.counter = counter;
+        }
+
+        [HttpGet]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View(counter);
+        }
+
+        [HttpPost]
+        [Route("index")]
+        public IActionResult Increase()
+        {
+            counter.Increase();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ week-7/day-3/CounterApp/CounterApp/Models/Counter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CounterApp.Models
+{
+    public class Counter
+    {
+        public int Counted { get; set; } = 1;
+
+        public void Increase()
+        {
+            Counted++;
+        }
+    }
+}
