264ba69a6db461b7aa5325a2189840934736a1b0
Tamas <deaktamas93@gmail.com>
2017-10-26 21:35:31 +0200

    program(w7-d4 greeting): functioning greeting method, design made in html for index page, greeting page is to be made too

---
+++ week-7/day-3/GreetingApp/GreetingApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GreetingApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GreetingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserInfo UserInfo;
+
+        public HomeController(UserInfo userinfo)
+        {
+            UserInfo = userinfo;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View(UserInfo);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult GetUserInfo(string userName)
+        {
+            UserInfo.Name = userName;
+            return RedirectToAction("Greeting");
+        }
+
+        [HttpGet]
+        [Route("Greeting")]
+        public IActionResult Greeting()
+        {
+            return View(UserInfo);
+        }
+    }
+}
---
+++ week-7/day-3/GreetingApp/GreetingApp/Models/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreetingApp.Models
+{
+    public class UserInfo
+    {
+        public string Name { get; set; }
+    }
+}
