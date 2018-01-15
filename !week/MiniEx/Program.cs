c9496da4a2ff69c64e87060a7358cedfcbca551e
Tamas <deaktamas93@gmail.com>
2018-01-15 09:47:37 +0100

    feat(Program): extend GetCommonLetters method with null handling

--- !week/MiniEx/Program.cs
+++ !week/MiniEx/Program.cs
@@ -8,12 +8,23 @@ namespace MiniEx
         static void Main(string[] args)
         {
             Console.WriteLine(GetCommonLetters("Green Fox Academy", "Gf A "));
+            Console.WriteLine(GetCommonLetters(null, "Gf A "));
+            Console.WriteLine(GetCommonLetters("", "Gf a "));
             Console.ReadLine();
         }
 
         private static char[] GetCommonLetters(string inputWord1, string inputWord2)
         {
-            return ((inputWord1.ToLower().Replace(" ", "").Intersect(inputWord2.ToLower().Replace(" ", ""))).ToArray());
+            if (inputWord1 != null && inputWord2 != null)
+            {
+                return ((inputWord1.ToLower().Replace(" ", "")
+                    .Intersect(inputWord2.ToLower().Replace(" ", "")))
+                    .ToArray());
+            }
+            else
+            {
+                return String.Empty.ToCharArray();
+            }
         }
     }
 }
