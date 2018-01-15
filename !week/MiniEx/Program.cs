0b3e1dad2beb1ad9a003b2ad3bd083a8aef88dec
Tamas <deaktamas93@gmail.com>
2018-01-15 09:32:12 +0100

    feat(Program): add GetCommonLetters function and call in console

---
+++ !week/MiniEx/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace MiniEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetCommonLetters("Green Fox Academy", "Gf A "));
+            Console.ReadLine();
+        }
+
+        private static char[] GetCommonLetters(string inputWord1, string inputWord2)
+        {
+            return ((inputWord1.ToLower().Replace(" ", "").Intersect(inputWord2.ToLower().Replace(" ", ""))).ToArray());
+        }
+    }
+}
